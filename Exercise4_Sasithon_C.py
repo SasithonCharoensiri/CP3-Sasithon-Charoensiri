2a01932b20d8b6218afa8b5828bca157bb06c361
SasithonCharoensiri <148411330+SasithonCharoensiri@users.noreply.github.com>
2023-10-19 10:27:13 +0700

    Add files via upload

---
+++ Exercise4_Sasithon_C.py
@@ -0,0 +1,9 @@
+Found_Eng = 80.5
+Gen_Bu = 78.6
+Com_System = 94.3
+Com_Pro = 87.9
+print("--- Your Score ---")
+print("Foundation English               :", Found_Eng)
+print("General Business                 :", Gen_Bu)
+print("Introduction to Computer Systems :", Com_System)
+print("Computer Programming             :", Com_Pro)
