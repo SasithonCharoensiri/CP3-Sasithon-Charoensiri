ce9ffc90f2bbc987274c36a8467acae3b6396899
SasithonCharoensiri <148411330+SasithonCharoensiri@users.noreply.github.com>
2023-10-30 15:13:49 +0700

    Add files via upload

---
+++ lecture50_Sasithon_C.py
@@ -0,0 +1,16 @@
+def addNumber(x,y):
+    print(x+y)
+
+def minusNumber(x,y):
+    print(x-y)
+
+def multipliedNumber(x,y):
+    print(x*y)
+
+def divideNumber(x,y):
+    print(x/y)
+
+addNumber(10,50)
+minusNumber(150,50)
+multipliedNumber(9,12)
+divideNumber(180,3)
